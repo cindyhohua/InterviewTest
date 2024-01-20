6683859777f6315d9f936bd4603d54305a29124c
賀華 <cindyhohua@livemail.tw>
2024-01-20 13:27:14 +0800

    git init

---
+++ InterviewTest_Cindy/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  InterviewTest_Cindy
+//
+//  Created by 賀華 on 2024/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ InterviewTest_CindyUITests/InterviewTest_CindyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  InterviewTest_CindyUITestsLaunchTests.swift
+//  InterviewTest_CindyUITests
+//
+//  Created by 賀華 on 2024/1/20.
+//
+
+import XCTest
+
+final class InterviewTest_CindyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
