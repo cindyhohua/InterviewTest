51f755d27d2f282adaaa1a6e32fd160b2e2e3c4c
賀華 <cindyhohua@livemail.tw>
2024-01-20 16:25:18 +0800

    feat: Add UIFont extension

---
+++ InterviewTest_Cindy/FontExtension.swift
@@ -0,0 +1,23 @@
+//
+//  FontExtension.swift
+//  InterviewTest_Cindy
+//
+//  Created by 賀華 on 2024/1/20.
+//
+
+import UIKit
+
+extension UIFont {
+    class var textStyle4: UIFont {
+        return UIFont(name: "PingFangTC-Medium", size: 17.0)!
+    }
+    class var textStyle3: UIFont {
+        return UIFont(name: "PingFangTC-Regular", size: 16.0)!
+    }
+    class var textStyle2: UIFont {
+        return UIFont(name: "PingFangTC-Medium", size: 13.0)!
+    }
+    class var textStyle: UIFont {
+        return UIFont(name: "PingFangTC-Regular", size: 13.0)!
+    }
+}
