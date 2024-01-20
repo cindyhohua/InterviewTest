f0b32e208979795692459e85a3b357652aab4579
賀華 <cindyhohua@livemail.tw>
2024-01-20 14:44:44 +0800

    feat: add tabbarController with storyboard

--- InterviewTest_Cindy/ViewController.swift
+++ InterviewTest_Cindy/ViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .brown
+        print("qqq")
     }
 
 
