0b52f00a69b98a900697ee50c97089865597f922
賀華 <cindyhohua@livemail.tw>
2024-01-20 14:53:52 +0800

    feat: Add colorExtension

--- InterviewTest_Cindy/FriendsViewController.swift
+++ InterviewTest_Cindy/FriendsViewController.swift
@@ -11,7 +11,7 @@ class FriendsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .brown
+        view.backgroundColor = UIColor.kokoPink
         print("qqq")
     }
 
